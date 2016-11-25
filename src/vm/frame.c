ba1a5cb6fd8edc11fbdb127007a6d1cf118df011
joonahn <sdeveloper@postech.ac.kr>
2016-11-25 18:10:28 +0900

    asdf

---
+++ src/vm/frame.c
@@ -0,0 +1,54 @@
+#include "vm/frame.h"
+
+void frame_table_init()
+{
+	int i;
+	frame_table = malloc(FRAME_TABLE_SIZE * sizeof(struct frame));
+	for (i = 0; i < FRAME_TABLE_SIZE; ++i)
+	{
+		frame_table[i].use_bit = 0;
+		frame_table[i].valid = 0;
+	}
+}
+
+
+uint32_t* frame_get_vaddr(struct frame* f)
+{
+	return f->vaddr;
+}
+
+uint32_t* frame_get_pagedir(struct frame* f)
+{
+	return f->pagedir;
+}
+
+bool frame_get_use_bit(struct frame* f)
+{
+	return f->use_bit;
+}
+
+bool frame_get_valid (struct frame* f)
+{
+	return f->valid ;
+}
+
+
+void frame_set_vaddr (struct frame* f, uint32_t* vaddr)
+{
+	f->vaddr = vaddr;
+}
+
+void frame_set_pagedir (struct frame* f, uint32_t * pagedir)
+{
+	f->pagedir = pagedir;
+}
+
+void frame_set_use_bit (struct frame* f, bool use_bit)
+{
+	f->use_bit = use_bit;
+}
+
+void frame_set_valid  (struct frame* f, bool valid)
+{
+	f->valid  = valid ;
+}
---
+++ src/vm/frame.h
@@ -0,0 +1,30 @@
+#ifndef VM_FRAME_H
+#define VM_FRAME_H
+
+#include "threads/thread.h"
+#include <stdint.h>
+#define FRAME_TABLE_SIZE 384
+
+struct frame *frame_table;
+
+struct frame {
+	uint32_t * vaddr;
+	uint32_t * pagedir;
+	bool use_bit;
+	bool valid;
+};
+
+void frame_table_init(void);
+
+uint32_t* frame_get_vaddr(struct frame*);
+uint32_t* frame_get_pagedir(struct frame*);
+bool frame_get_use_bit(struct frame*);
+bool frame_get_valid (struct frame*);
+
+void frame_set_vaddr(struct frame*, uint32_t*);
+void frame_set_pagedir(struct frame*, uint32_t*);
+void frame_set_use_bit(struct frame*, bool);
+void frame_set_valid (struct frame*, bool);
+
+
+#endif
